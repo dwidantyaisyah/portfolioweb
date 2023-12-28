e634b30a49fd0d8f961e034adb86e8ff5a4bc794
Dasyaaaaaaaaaa1 <dantyaisyah17@gmail.com>
2023-12-28 23:01:52 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const NavBarNav = document.querySelector('.NavBar-nav');
+document.querySelector('#titiktiga-menu').onclick = () => {
+NavBarNav.classList.toggle('active');
+};
+
+
